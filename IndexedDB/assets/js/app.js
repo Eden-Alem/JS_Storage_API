f96156639de54f286067c1a42e4f01ed063b0759
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-11 05:12:31 +0300

    Created the database of indexedDB and handled events inside an event listener that is called when its loaded due to indexedDB's asynchronous behavior

--- IndexedDB/assets/js/app.js
+++ IndexedDB/assets/js/app.js
@@ -9,3 +9,61 @@ const desc = document.querySelector('#drop');
 let isAscending = true;
 
 let DB;
+
+document.addEventListener('DOMContentLoaded', () => {
+    let TasksDB = indexedDB.open('tasks', 2);
+    TasksDB.onsuccess = function() {
+        console.log(`Database is ready`);
+        DB = TasksDB.result;
+
+        console.log(DB);
+        displayTaskList();
+        
+    }
+
+    TasksDB.onerror = function() {
+        console.log(`An error occured`);
+    }
+
+    //on upgrade or after version is changed
+    TasksDB.onupgradeneeded = function(e) {
+        console.log(`On upgrade`);
+        let db = e.target.result;
+
+        let objectStore = db.createObjectStore('tasks', {keyPath:'id', autoIncrement: true});
+
+        objectStore.createIndex('taskname', 'taskname', {unique: false});
+        console.log(`Its ready`);
+
+    }
+
+    form.addEventListener('submit', addNewTask);
+
+    function addNewTask(e) {
+        e.preventDefault(); 
+
+        if (taskInput.value === "") {
+            taskInput.style.borderColor = "red";
+            return;
+        }
+        // create a new object with the form info
+        let newTask = {
+            taskname: taskInput.value
+        }
+        // Insert the object into the database
+        let transaction = DB.transaction(['tasks'], 'readwrite');
+        let objectStore = transaction.objectStore('tasks');
+        let request = objectStore.add(newTask);
+        // on success
+        request.onsuccess = () => {
+            form.reset();
+        }
+        transaction.oncomplete = () => {
+            console.log('New Task added');
+            displayTaskList();
+        }
+        transaction.onerror = () => { console.log('There was an error, try again!'); }
+    }
+
+
+});
