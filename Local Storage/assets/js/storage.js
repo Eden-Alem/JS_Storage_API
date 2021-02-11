32044215a177d22f6e21d37c98b722ec3da71c0d
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-11 04:58:26 +0300

    Created a js file named storage and added a function that adds text input to the local storage as an array

---
+++ Local Storage/assets/js/storage.js
@@ -0,0 +1,10 @@
+function addToDatabase(newTask) {
+    let listofTasks;
+    if(localStorage.getItem("tasks") == null) {
+        listofTasks = [];
+    } else {
+        listofTasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    listofTasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(listofTasks));
+}
