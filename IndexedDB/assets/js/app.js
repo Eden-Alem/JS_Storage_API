5a906c6e07c887d922b0da1b859e14c0011afdd4
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-11 05:08:07 +0300

    Created the js file named app in indexedDB implementation folder and declared the UIs

---
+++ IndexedDB/assets/js/app.js
@@ -0,0 +1,11 @@
+const taskInput = document.querySelector('#task'); //the task input text field
+const form = document.querySelector('#task-form'); //The form at the top
+const filter = document.querySelector('#filter'); //the task filter text field
+const taskList = document.querySelector('.collection'); //The UL
+const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
+const reloadIcon = document.querySelector('.fa');
+const desc = document.querySelector('#drop');
+
+let isAscending = true;
+
+let DB;
